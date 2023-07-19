c0e0f045d869fe6929a94c38ad2957bdc79174e8
ValchukA <andva02@gmail.com>
2023-07-20 01:00:40 +0300

    Remove unnecessary using

--- ToDoApp.Storage.MongoDb/Usings.cs
+++ ToDoApp.Storage.MongoDb/Usings.cs
@@ -2,7 +2,6 @@
 global using AutoMapper;
 global using FluentValidation;
 global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.DependencyInjection.Extensions;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 global using MongoDB.Driver;
