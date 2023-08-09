089d97b53adcba525095f71366db3c49f9042a21
ValchukA <andva02@gmail.com>
2023-08-10 01:55:54 +0300

    Substitute Moq with NSubstitute

--- backend/ToDoApp.Api.Tests/Usings.cs
+++ backend/ToDoApp.Api.Tests/Usings.cs
@@ -1,6 +1,6 @@
 global using FluentValidation.TestHelper;
 global using Microsoft.Extensions.Options;
-global using Moq;
+global using NSubstitute;
 global using ToDoApp.Api.Contracts.Requests;
 global using ToDoApp.Api.Options;
 global using ToDoApp.Api.Validation;
